1571f45a16db1bd968fbe9fc99a1d2edf7ac7926
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2021-02-19 23:17:35 +1100

    Fixed background rotation

--- Assets/Scripts/FloorRotation.cs
+++ Assets/Scripts/FloorRotation.cs
@@ -7,6 +7,6 @@ public class FloorRotation : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), rotationSpeed * Time.deltaTime);
+		transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
 	}
 }
